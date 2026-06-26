b4f01d40bb196fd319b62c114a1508ff027e4297
SJWakhare <wshivani123@gmail.com>
2026-06-26 22:11:21 +0530

    Organizing code into modules. Importing functions from one file to another

--- python-for-ai/sales-analysis/analyzer.py
+++ python-for-ai/sales-analysis/analyzer.py
@@ -32,3 +32,24 @@ print("\nFiles saved:")
 print("- output/sales_data.json")
 print("- output/sales_data.xlsx") 
 print("- output/sales_with_totals.csv")
+
+
+
+
+
+# # Loading different files:
+# # CSV
+# df = pd.read_csv('data/file.csv')
+
+# # JSON
+# df = pd.read_json('data/file.json')
+# # or for simple JSON:
+# with open('data/config.json', 'r') as f:
+#     data = json.load(f)
+
+# # Excel
+# df = pd.read_excel('data/file.xlsx')
+
+# # Text files
+# with open('data/file.txt', 'r') as f:
+#     text = f.read()
